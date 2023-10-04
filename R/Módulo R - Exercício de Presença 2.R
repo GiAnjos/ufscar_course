69fd81d8a8102322ff420ed580c393b894d25022
Giovana Dos Anjos <93729823+GiAnjos@users.noreply.github.com>
2023-10-04 18:16:44 -0300

    Add files via upload

---
+++ R/Módulo R - Exercício de Presença 2.R
@@ -0,0 +1,12 @@
+# Crie uma sequência do seu ano de nascimento até 2023 e responda:
+vetor.numerico = c(2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021,2022,2023)
+#ou
+vetor.numerico = seq(from = 2000, to = 2023, by = 1)
+# (a) Qual é a posição do número 2020?
+ ## Resposta = vetor.numerico[21] = posição 21
+
+# (b) Quantos elementos existem nesse vetor?
+ ## Resposta = 24 elementos
+
+# (c) Qual é a diferença entre o último e o primeiro elemento desse vetor?
+ ## Resposta = 23
